28af5c736104f246a6a797b8bfd708b00e3bef73
sergey <sergey@gmail.com>
2024-03-01 17:16:03 +0300

    Fix tests with 100

--- test.py
+++ test.py
@@ -11,5 +11,5 @@ test_calculate(10,20, 30)
 test_calculate(1,15, 16)
 test_calculate(40,150, 190)
 
-test_calculate(0, 100, 101)
+test_calculate(0, 100, 100)
 test_calculate(sys.float_info.max, 1, sys.float_info.max)
