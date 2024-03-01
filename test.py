e095ac5eb5d6615152e002cf242705fbb402acdf
sergey <sergey@gmail.com>
2024-03-01 17:11:23 +0300

    Add broken test

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
+import sys
 from calc import calculate as cal
 
-def test_calculate(a,b):
-    c = cal(a,b)
+def test_calculate(a,b, expected):
+    actual = cal(a,b)
 
-    test_c = a+b
-
-    assert c == test_c
+    assert actual == expected
 
 
-test_calculate(10,20)
-test_calculate(1,15)
-test_calculate(40,150)
+test_calculate(10,20, 30)
+test_calculate(1,15, 16)
+test_calculate(40,150, 190)
 
-test_calculate(0, 1E-30)
+test_calculate(0, 100, 101)
+test_calculate(sys.float_info.max, 1, sys.float_info.max)
